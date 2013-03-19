b322cb4f1d46e7d99dcb9e80efaecf5b2cda0259
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-19 17:57:30 -0400

    Added balance number in the nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     nav = ""
 
     if @auth.present?
-      nav += "<li>#{link_to(@auth.name, '/login', :method => :delete, :remote => true, :class => 'button large alert')}</li>"
+      nav += "<li>#{link_to(@auth.name + ' - ' + number_to_currency(@auth.banks.map(&:balance).reduce(:+)).to_s, '/login', :method => :delete, :remote => true, :class => 'button large alert')}</li>"
     else
       nav += "<li>#{link_to('Login', '/login', :remote => true, :class => 'button large radius')}"
     end
