658b18976e5e7c7b80b869073aca7177fdf6ca4d
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-19 09:27:58 -0400

    Added ajax login on the welcome index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_nav
+    nav = ""
+
+    if @auth.present?
+      nav += "<li>#{link_to(@auth.name, '/login', :method => :delete, :remote => true, :class => 'button tiny alert')}</li>"
+    else
+      nav += "<li>#{link_to('Login', '/login', :remote => true, :class => 'button tiny radius')}"
+    end
+
+    nav
+  end
 end
