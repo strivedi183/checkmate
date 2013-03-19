7c27b511734bd99f09d23b4e03995a63d29b2f67
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-19 16:19:44 -0400

    Factory Girl issues fixed

--- spec/factories/user_spec.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user, class: User do
-    name                     'Dick'
-    email                    'dick@gmail.com'
-    password                 'a'
-    password_confirmation    'a'
-  end
-end
