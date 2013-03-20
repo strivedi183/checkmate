bedcb2143bfc4a0b2164573a8067e853a2b34e0f
Tony Contreras <tony.contreras@gmail.com>
2013-03-19 23:44:29 -0400

    pulling for lulz

--- spec/features/shared/login_helper.rb
+++ spec/features/shared/login_helper.rb
@@ -4,6 +4,6 @@ module LoginHelper
     click_link('Login')
     fill_in('Email', :with => user.email)
     fill_in('Password', :with => user.password)
-    click_button('Login Checkmate')
+    click_button('Login $ for $')
   end
 end
